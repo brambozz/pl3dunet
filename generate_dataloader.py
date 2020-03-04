ab4529c7675070463cc7af23ba3d4b437f349f24
Bram de Wilde <bramdewilde@outlook.com>
2020-03-04 16:46:37 +0100

    Finalize generate_dataloader.py

--- generate_dataloader.py
+++ generate_dataloader.py
@@ -1,18 +1,71 @@
-import torch
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import generate_data
 
-# Generate as follows
 
-# Load single image into memory
+class GeometricObjectsDataset(Dataset):
+    """Dataset of 3D images containing random geometric objects"""
 
-# Apply mirroring strategy, find some premade code for this
+    def __init__(self, epoch_size=100, image_size=28, num_obj=4, input_size=116):
+        """
+        Parameters
+        ----------
+        epoch_size : int
+            Size of one epoch
+        image_size : int
+            Size of the image
+        input_size : int
+            Size of input to the network
+        num_obj : int
+            Number of objects in each image
+        """
+        self.epoch_size = epoch_size
+        self.image_size = image_size
+        self.input_size = input_size
+        self.num_obj = num_obj
 
-# Use 'unfold' to break up the image into non-overlapping patches
-x = torch.randn(500, 500, 496)
-x = x.unfold(0, 132, 132)
-x = x.unfold(1, 132, 132)
-x = x.unfold(2, 116, 116)
-x = x.contiguous().view(-1, 132, 132, 116)
+    def __len__(self):
+        return self.epoch_size
 
-# Simililarly for the masks
+    def __getitem__(self, idx):
+        # Generate random sample
+        sample = generate_data.generate_sample(1, self.image_size, self.num_obj)
+        image, masks = (
+            sample["image"].astype(np.float32),
+            sample["masks"].astype(np.float32),
+        )
 
-# Stack all patches in the batch dimension and return item
+        # Reflection pad image and masks to input size
+        padding = (self.input_size - self.image_size) // 2
+        input = np.pad(image, padding, mode="reflect")
+
+        # Add channel dimension to image and input
+        image = image[None, :, :, :]
+        input = input[None, :, :, :]
+
+        return {"image": image, "input": input, "masks": masks}
+
+
+def get_dataloader(
+    batch_size=1, epoch_size=100, image_size=28, num_obj=4, input_size=116
+):
+    """Create dataloader for geometric objects dataset
+
+    Parameters
+    ----------
+    batch_size : int
+    epoch_size : int
+        Size of one epoch
+    image_size : int
+        Size of the image
+    input_size : int
+        Size of input to the network
+    num_obj : int
+        Number of objects in each image
+
+    Returns
+    -------
+    dataloader : torch.utils.data.DataLoader
+    """
+    dataset = GeometricObjectsDataset(input_size)
+    return DataLoader(dataset, batch_size=batch_size)
